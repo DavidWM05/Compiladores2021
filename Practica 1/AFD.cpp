36a5eece27fc6756c52166b51781b271e6ded740
DavidWM05 <83983647+DavidWM05@users.noreply.github.com>
2021-08-16 18:00:39 -0500

    Create AFD.cpp

---
+++ Practica 1/AFD.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+Peralta Gonzalez Luis David
+Compiladores
+
+*******************************************************************************/
+
+#include <iostream>
+#include <vector>
+#define MAX 100
+
+using namespace std;
+
+class AFN{
+    //Atributos de la clase AFN
+        vector<int> Q;          //Estados
+        vector<char> Alfabeto;  //Alfabeto
+        int q0;                 // Estado inicial
+        vector<int>qf;          // Estados final
+        int Transiciones[MAX][MAX]; //Transiciones
+
+    //Funciones
+    public:
+        void iniciarAutomata(){
+            cout << "Iniciando automata AFN" << endl;
+        }
+    };
+
+int main()
+{
+
+    AFN obj;
+    obj.iniciarAutomata();
+
+    return 0;
+}
